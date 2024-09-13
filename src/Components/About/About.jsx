1527d68fe14311719de9264c5bfe34916361bbc1
justinait <justiiturraspe@gmail.com>
2024-09-13 14:00:58 -0300

    icons about

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
+import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
+import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+import VolunteerActivismOutlinedIcon from '@mui/icons-material/VolunteerActivismOutlined';
+import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
+import TaskAltOutlinedIcon from '@mui/icons-material/TaskAltOutlined';
+
+function About() {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,18 +5,18 @@ import Techniques from '../Techniques/Techniques'
 function Home() {
   return (
     <div>
-        <div className='heroContainer'>
-            <div className='hero'>
-                <h1>Centro Profesional</h1>
-                <h5>Proyecto y apertura</h5>
-            </div>
-            <div className='heroFooter'>
-                <p className='heroFooterStrong'>Limpieza profesional y precisión.</p>
-                <p className='heroFooterP'>Tu vehículo en su mejor versión</p>
-            </div>
+      <div className='heroContainer'>
+        <div className='hero'>
+          <h1>Centro Profesional</h1>
+          <h5>Proyecto y apertura</h5>
         </div>
-        
-        < Techniques />
+        <div className='heroFooter'>
+          <p className='heroFooterStrong'>Limpieza profesional y precisión.</p>
+          <p className='heroFooterP'>Tu vehículo en su mejor versión</p>
+        </div>
+      </div>
+      
+      < Techniques />
     </div>
   )
 }
