00549e5f9722624e494655ea15181802e1dd43cd
justinait <justiiturraspe@gmail.com>
2024-09-13 11:12:15 -0300

    hero

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import '@fontsource-variable/montserrat';
+import Home from "./Components/Home/Home"
+import Navbar from "./Components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+        <div className='hero'>
+            <h1>Centro Profesional</h1>
+            <h5>Proyecto y apertura</h5>
+        </div>
+        <div className='heroFooter'>
+            <p className='heroFooterStrong'>Limpieza profesional y precisión.</p>
+            <p className='heroFooterP'>Tu vehículo en su mejor versión</p>
+             
+        </div>
+    </div>
+  )
+}
+
+export default Home
