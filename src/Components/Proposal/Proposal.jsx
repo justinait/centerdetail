431477338668054213f6c78bb0634ca3c76c09fb
justinait <justiiturraspe@gmail.com>
2024-09-16 10:33:02 -0300

    proposal

--- src/Components/Proposal/Proposal.jsx
+++ src/Components/Proposal/Proposal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Proposal.css'
+import car from '/images/car.png'
 
 function Proposal() {
   return (
@@ -9,6 +10,18 @@ function Proposal() {
             Nuestra propuesta de valor se basa en seis pilares fundamentales que definen y diferencian nuestro negocio en el mercado. 
             Estos elementos aseguran que cada cliente reciba una experiencia única y excepcional en cada interacción con nuestra marca.
         </p>
+
+        <div className='carProposalBox'>
+            <img src={car} alt="CAR" className='carProposal' />
+            <div className='itemsProposalBox'>
+                <p> <strong>1.</strong> Experiencia <strong> completa</strong></p>
+                <p> <strong>2.</strong> Servicios <strong> especializados</strong></p>
+                <p> <strong>3.</strong> Hacemos llegar <br /><strong> el producto</strong></p>
+                <p> <strong>4.</strong> Marketing y <strong> estrategias de venta</strong></p>
+                <p> <strong>5.</strong> <strong>Innovación</strong> <br /> y tendencias</p>
+                <p> <strong>6.</strong> Asesoramineto y <strong> soporte permanente</strong></p>
+            </div>
+        </div>
     </div>
   )
 }
