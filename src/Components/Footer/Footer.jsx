113e456003a7e54041e00c4f2c2cfd3abe445ed7
justinait <justiiturraspe@gmail.com>
2024-09-18 19:00:02 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Footer.css'
+import ig from '/ig.png'
 
 function Footer() {
   return (
     <div className='footerContainer'>
-        <h2>Contacto</h2>
-        
-        <p className='footerSubtitle'>¿Listo para dar el siguiente paso?</p>
-        <p className='footerText'>Estamos disponibles para cualquier consulta, no dudes en comunicarte con nosotros</p>
+      <h2>Contacto</h2>
+      
+      <p className='footerSubtitle'>¿Listo para dar el siguiente paso?</p>
+      <p className='footerText'>Estamos disponibles para cualquier consulta, no dudes en comunicarte con nosotros</p>
+
+      <div>
+        <img src={ig} alt="" />
+        <p>@centerdetail</p>
+      </div>
     </div>
   )
 }
