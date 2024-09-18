c7429fadb83535faa5bd9db66bec03ea53114e2c
justinait <justiiturraspe@gmail.com>
2024-09-18 18:11:17 -0300

    css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import logo from '/logowhite.png'
+import logo from '/isologo.png'
 import './Navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
   return (
     <div className='navbarContainer'>
       <img src={logo} alt="" className='logo'/>
-
+      <MenuIcon className='burgerMenu'/>
     </div>
   )
 }
