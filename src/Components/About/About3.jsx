119b2356f9ea7d1f5bf26bc58b7e675826971a47
justinait <justiiturraspe@gmail.com>
2024-09-26 11:05:08 -0300

    tablet 1000

--- src/Components/About/About3.jsx
+++ src/Components/About/About3.jsx
@@ -5,30 +5,34 @@ import AutoGraphOutlinedIcon from '@mui/icons-material/AutoGraphOutlined';
 import RocketLaunchOutlinedIcon from '@mui/icons-material/RocketLaunchOutlined';
 
 function About3() {
+
   return (
     <div className='objetivosContainer' id='objetivos'>
-        <h2>Objetivos</h2>
-        <div>
-            <h6 className='objetivosTitle'> <AutoFixHighOutlinedIcon className='objetivosIcon' /> Servicio de excelencia</h6>
-            <p className='objetivosText'>Nos enfocaremos en ofrecer un servicio de excelencia que supere las expectativas de nuestros clientes, 
-                con atención personalizada y técnicas de vanguardia para asegurar resultados impecables en cada visita.
-            </p>
-        </div>
+            <h2>Objetivos</h2>
+        <div className='objetivosBoxesContainer'>
 
-        <div>
-            <h6 className='objetivosTitle'> <AutoGraphOutlinedIcon className='objetivosIcon' /> Crecimiento sostenible</h6>
-            <p className='objetivosText'>Buscamos un crecimiento sostenible, enfocándonos en relaciones duraderas con los clientes y creando 
-                un ambiente acogedor. Con estrategias de marketing efectivas y una gestión eficiente, aspiramos a consolidarnos como líderes 
-                en el sector del detailing y wrapping.
-            </p>
-        </div>
+            <div>
+                <h6 className='objetivosTitle firstBorder'> <AutoFixHighOutlinedIcon className='objetivosIcon' /> Servicio de excelencia</h6>
+                <p className='objetivosText'>Nos enfocaremos en ofrecer un servicio de excelencia que supere las expectativas de nuestros clientes, 
+                    con atención personalizada y técnicas de vanguardia para asegurar resultados impecables en cada visita.
+                </p>
+            </div>
 
-        <div>
-            <h6 className='objetivosTitle'> <RocketLaunchOutlinedIcon className='objetivosIcon' /> Expansión</h6>
-            <p className='objetivosText'>Nuestra visión se centra en la expansión estratégica, con el objetivo de crecer en nuevos mercados 
-                y ampliar nuestra presencia regional o nacional. A través de una cuidadosa selección de ubicaciones y capacitación de personal 
-                cualificado, buscamos llevar la excelencia de nuestros servicios a un público más amplio.
-            </p>
+            <div>
+                <h6 className='objetivosTitle'> <AutoGraphOutlinedIcon className='objetivosIcon' /> Crecimiento sostenible</h6>
+                <p className='objetivosText'>Buscamos un crecimiento sostenible, enfocándonos en relaciones duraderas con los clientes y creando 
+                    un ambiente acogedor. Con estrategias de marketing efectivas y una gestión eficiente, aspiramos a consolidarnos como líderes 
+                    en el sector del detailing y wrapping.
+                </p>
+            </div>
+
+            <div>
+                <h6 className='objetivosTitle lastBorder'> <RocketLaunchOutlinedIcon className='objetivosIcon' /> Expansión</h6>
+                <p className='objetivosText'>Nuestra visión se centra en la expansión estratégica, con el objetivo de crecer en nuevos mercados 
+                    y ampliar nuestra presencia regional o nacional. A través de una cuidadosa selección de ubicaciones y capacitación de personal 
+                    cualificado, buscamos llevar la excelencia de nuestros servicios a un público más amplio.
+                </p>
+            </div>
         </div>
     </div>
   )
