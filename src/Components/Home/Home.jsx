f8a8ffbfa88c90aca1dea1fd1e888fca15fabe50
justinait <justiiturraspe@gmail.com>
2024-09-26 16:52:37 -0300

    hero

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,23 +3,36 @@ import './Home.css'
 import Techniques from '../Techniques/Techniques'
 import About from '../About/About'
 import Proposal from '../Proposal/Proposal'
-import About2 from '../About/About2'
 import About3 from '../About/About3'
 import Budget from '../Budget/Budget'
 import Footer from '../Footer/Footer'
 import logo from '/logowhite.png'
+import lavar from '/images/vinilohero.jpg'
+import vinilo from '/images/car.png'
+import pulir from '/images/pulirhero.jpg'
+
 
 function Home() {
   return (
     <div>
       <div className='heroContainer' id='inicio'>
-        <div className='hero'>
-          <img src={logo} alt="" className='heroLogo' />
-          <div className='heroTextContainer'>
-            <h1>Centro Profesional</h1>
-            <h5>Franquicias de detallado de vehículos</h5>
+        <div>
+
+          <div className='heroImageDiv'>
+            <img src={lavar} alt="" className='imagesHero'/>
+            <img src={vinilo} alt="" className='imagesHero'/>
+            <img src={pulir} alt="" className='imagesHero' style={{objectPosition: '40%'}}/>
+          </div>
+
+          <div className='heroInfo'>
+            <img src={logo} alt="" className='heroLogo' />
+            <div className='heroTextContainer'>
+              <h1>Centro Profesional</h1>
+              <h5>Franquicias de detallado de vehículos</h5>
+            </div>
           </div>
         </div>
+
         <div className='heroFooter'>
           <p className='heroFooterStrong'>Donde cada coche cuenta una historia</p>
           <p className='heroFooterP'>y tu negocio escribe el próximo capítulo.</p>          
